359aaea3de82deb7ebd62ea5132f21edb931068c
liaohuqiu <liaohuqiu@gmail.com>
2018-06-24 23:50:10 +0800

    add lesson3

--- manager.sh
+++ manager.sh
@@ -130,7 +130,7 @@ function attach() {
 function run_tests() {
     restart
     local cmd='ganache-cli > /dev/null'
-    cmd="$cmd & sleep 2; cd /opt/src/lesson-1"
+    cmd="$cmd & sleep 2; cd /opt/src/lesson-3"
     cmd="$cmd; truffle test"
     local cmd="docker exec $docker_run_fg_mode $container_name bash -c '$cmd'"
     run_cmd "$cmd"
